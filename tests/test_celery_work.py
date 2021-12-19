a4bcf3d4ed4a2e110e08d70168f5dc82aaa55143
kangprog <thtjth123@naver.com>
2021-12-19 22:20:25 +0900

    feature: usefixtures 추가

--- tests/test_celery_work.py
+++ tests/test_celery_work.py
@@ -1,3 +1,4 @@
+import pytest
 from tasks.celery_task import work_task
 
 
@@ -6,12 +7,13 @@ from tasks.celery_task import work_task
 # https://medium.com/@scythargon/how-to-use-celery-pytest-fixtures-for-celery-intergration-testing-6d61c91775d9
 #
 
-
-def test_celery_work_return_one(celery_app, celery_worker):
+@pytest.mark.usefixtures('celery_session_app')
+@pytest.mark.usefixtures('celery_session_worker')
+def test_celery_work_return_one():
     assert work_task.delay(1).get(timeout=10) == 1
     assert work_task.apply_async(args=[1]).get(timeout=10) == 1
 
 
-def test_celery_work_return_two(celery_app, celery_worker):
+def test_celery_work_return_two():
     assert work_task.delay(2).get(timeout=10) == 2
     assert work_task.apply_async(args=[2]).get(timeout=10) == 2
